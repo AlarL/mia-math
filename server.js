53cc53aef72fb9a22eddb1186ab2a14c6307a1c3
AlarL <alar@loquiz.com>
2025-02-27 10:13:22 +0200

    Parandatud server.js - eemaldatud duplikaadid

--- server.js
+++ server.js
@@ -1,19 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.send("Chat Assistant töötab!");
-});
-
-app.listen(PORT, () => console.log(`Server töötab pordil ${PORT}`));
-const express = require("express");
-const cors = require("cors");
 const path = require("path");
 
 const app = express();
@@ -22,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Teenindab index.html lehte
+// Teenindab `index.html` lehte
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
